d1a9a05f84bb8b6db7352d7cee355d3604a6a622
Peter Moresi <peter.moresi@gmail.com>
2014-10-22 22:10:08 -0700

    Updates to the page

--- loan-calculator.js
+++ loan-calculator.js
@@ -1,14 +1,58 @@
 
 (function() {
     
-    <<getDataSet>>
-
-    <<reloadTable>>
-
-    <<reloadGraph>>
+    function getDataSet() { 
+        var output = {};
+        output.loan_amount = parseFloat( $('#loan_amount').val() );
+        output.interest_rate = parseFloat( $('#interest_rate').val() );
+        output.payments_per_year = parseInt( $('#payments_per_year').val() );
+        output.years = parseInt( $('#years').val() );
         
-    <<handleKeyPress>>
+        output.payment = pmt(interest_rate/100/payments_per_year, payments_per_year * years, -loan_amount);
+        
+        output.schedule = compute_schedule( output.loan_amount,
+                                            output.interest_rate,
+                                            output.payments_per_year,
+                                            output.years,
+                                            output.payment );
     
-    <<handlePageReady>>
+        return output;
+    }
+    
+
+    function reloadTable(ds) {
+        $('#schedule').empty();
+        $('#schedule').html( '<table cellpadding="0" cellspacing="0" border="0" class="display table" id="schedule_table"></table>' );
+        $('#schedule_table').dataTable( {
+            "data": ds.schedule,
+            "searching": false,
+            "columns": [
+                { "title": "Period" },
+                { "title": "Principle" },
+                { "title": "Interest" },
+                { "title": "Remaining" }
+            ],
+            "search": false,
+            "paging":   false,
+            "ordering": false,
+            "info":     false
+        } );   
+    }
+
+    function reloadGraph(dataSet) {
+        
+    }
+        
+    $(document).on('keyup', '.user-input', function(e) {
+        var ds = getDataSet();
+    
+        $('#payment_amount').text('$' + payment.toFixed(2));
+        reloadTable(ds);
+        reloadGraph(ds);
+    });
+    
+    $(document).ready(function() {
+        reloadTable();
+    });
 
 })();
